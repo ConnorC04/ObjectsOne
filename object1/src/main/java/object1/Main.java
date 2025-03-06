a78b16d99cd1ac4e3a26def29fe7f080d8dd2026
ConnorC04 <connorjcasey@iCloud.com>
2025-03-06 12:35:11 -0500

    finished?

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,15 +5,21 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
-//        Logger logger=Logger.getLogger(Main.class.getName());
-//        logger.info("This is a module-using Hello World!");
+//      Logger logger=Logger.getLogger(Main.class.getName());
+//      logger.info("This is a module-using Hello World!");
 
+        Person[] people = new Person[10];
 
-            // Declare and initialize an object
-            Person person1 = new Person("John", 30, 72);
+        // Declare and initialize an object
+        Person person1 = new Person("Alice", 25, 68);
+        Person person2 = new Person("Bob", 30, 72);
 
-            /* Now you can use the Object `person1` */
-        System.out.println(person1.name);
+        people[0] = person1;
+        people[1] = person2;
+
+        /* Now you can use the Object `person1` */
+        Person person = people[0];
+        System.out.println(person);
 
     }
 }
